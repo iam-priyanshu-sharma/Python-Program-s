3cecc6a3e5b96a89b45fda806175acc48f6aa3c8
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-06-25 17:24:23 +0530

    Project name changed

--- Day 1 Project Band Name Generator/main.py
+++ Day 1 Project Band Name Generator/main.py
@@ -1,4 +1,4 @@
-print("Welcome to the Band Name Generator.")
+print("Welcome to the Day 1 Project Band Name Generator.")
 street = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print("Your band name could be " + street + " " + pet)
