f50691ee635f55c89cf3281992fb8c4098483e8d
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-06-29 09:52:00 +0530

    Exercise 15

---
+++ Exercise 15 - Treasure Map/main.py
@@ -0,0 +1,15 @@
+row1 = ["⬜️", "️⬜️", "️⬜️"]
+row2 = ["⬜️", "⬜️", "️⬜️"]
+row3 = ["⬜️️", "⬜️️", "⬜️️"]
+maps = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+# maps[vertical - 1][horizontal - 1] = "X"
+selected_row = maps[vertical - 1]
+selected_row[horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
