7a9bdcd0e4a23f7e1819e287475557e24f9589cd
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-08-03 10:40:29 +0530

    fix bug

--- Day 26 Project - NATO Alphabet/main.py
+++ Day 26 Project - NATO Alphabet/main.py
@@ -2,7 +2,6 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
 word = input("Enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
