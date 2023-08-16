c8af9882b65d8407960816aaa725b2bc1470ef08
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-08-16 11:02:19 +0530

    Day 37 Project

---
+++ Day 37 Project - Habit Tracking/main.py
@@ -0,0 +1,67 @@
+import requests
+from datetime import datetime
+
+USERNAME = "bholabharti"
+TOKEN = "br06poiuy74hg"
+GRAPH_ID = "graph02"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# POST
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Coding Graph",
+    "unit": "Hr",
+    "type": "float",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%d/%m/%Y"),
+    "quantity": input("How many Hours did you code today? "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%d/%m/%Y')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+# DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
