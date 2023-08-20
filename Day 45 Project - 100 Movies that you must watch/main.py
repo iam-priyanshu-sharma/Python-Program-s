f79d293a06b53e3044bb93887c74da1f76f10d12
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-08-20 10:45:19 +0530

    Day 45 Project

---
+++ Day 45 Project - 100 Movies that you must watch/main.py
@@ -0,0 +1,50 @@
+# import requests
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+WEB_FILE = "./data/100_best_movies.html"
+
+
+# Using requests_html to render JavaScript
+def get_web_page():
+    # create an HTML Session object
+    session = HTMLSession()
+    # Use the object above to connect to needed webpage
+    response = session.get(URL)
+    # Run JavaScript code on webpage
+    response.html.render()
+
+    # Save web page to file
+    with open(WEB_FILE, mode="w", encoding="utf-8") as fp:
+        fp.write(response.html.html)
+
+
+def read_web_file():
+    try:
+        open(WEB_FILE)
+    except FileNotFoundError:
+        get_web_page()
+    finally:
+        # Read the web page from file
+        with open(WEB_FILE, mode="r", encoding="utf-8") as fp:
+            content = fp.read()
+        return BeautifulSoup(content, "html.parser")
+
+
+# Read web file if it exists, load from internet if it doesn't exist
+# result = read_web_file()
+# response = requests.get(URL)
+# website_html = response.text
+
+soup = read_web_file()
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
