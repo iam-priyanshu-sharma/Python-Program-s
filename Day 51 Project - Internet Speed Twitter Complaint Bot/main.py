b1c7e5115319d46cab132d0a7762556b4a20e6d9
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2023-08-24 10:14:30 +0530

    Fixed all problems and bugs

--- Day 51 Project - Internet Speed Twitter Complaint Bot/main.py
+++ Day 51 Project - Internet Speed Twitter Complaint Bot/main.py
@@ -5,7 +5,7 @@ import time
 
 PROMISED_DOWN = 100
 PROMISED_UP = 100
-TWITTER_EMAIL = "YOUR TWITTER EMAIL"
+TWITTER_EMAIL = "YOUR TWITTER EMAIL OR USERNAME"
 TWITTER_PASSWORD = "YOUR TWITTER PASSWORD"
 
 
@@ -18,48 +18,55 @@ class InternetSpeedTwitterBot:
     def get_internet_speed(self):
         self.driver.get("https://www.speedtest.net/")
 
-        accept_button = self.driver.find_elements(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
+        accept_button = self.driver.find_element(By.ID, "onetrust-accept-btn-handler")
         accept_button.click()
 
         time.sleep(3)
 
-        go_button = self.driver.find_elements(By.CSS_SELECTOR, ".start-button a")
+        go_button = self.driver.find_element(By.CSS_SELECTOR, ".start-button a")
         go_button.click()
         time.sleep(60)
-        self.up = self.driver.find_elements(By.XPATH,
-                                            '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div['
-                                            '3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-        self.down = self.driver.find_elements(By.XPATH,
-                                              '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div['
-                                              '3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+        self.up = self.driver.find_element(By.XPATH,
+                                           '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div['
+                                           '3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.down = self.driver.find_element(By.XPATH,
+                                             '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div['
+                                             '3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
 
     def tweet_at_provider(self):
         self.driver.get("https://twitter.com/login")
         time.sleep(2)
-        email = self.driver.find_elements(By.XPATH,
-                                          '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div['
-                                          '1]/label/div/div[2]/div/input')
-        password = self.driver.find_elements(By.XPATH,
-                                             '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div['
-                                             '2]/label/div/div[2]/div/input')
+        email = self.driver.find_element(By.XPATH,
+                                         '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div['
+                                         '2]/div/div/div/div[5]/label/div/div[2]/div/input')
         email.send_keys(TWITTER_EMAIL)
+
+        next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div['
+                                                         '2]/div/div/div[2]/div[2]/div/div/div/div[6]/div/span/span')
+        next_button.click()
+        time.sleep(2)
+
+        password = self.driver.find_element(By.XPATH,
+                                            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div['
+                                            '2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+
         password.send_keys(TWITTER_PASSWORD)
         time.sleep(2)
         password.send_keys(Keys.ENTER)
         time.sleep(5)
-        tweet_compose = self.driver.find_elements(By.XPATH,
-                                                  '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div['
-                                                  '2]/div/div[2]/div[1]/div/div/div/div[2]/div['
-                                                  '1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div['
-                                                  '2]/div/div/div/div')
+        tweet_compose = self.driver.find_element(By.XPATH,
+                                                 '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div['
+                                                 '2]/div/div[2]/div[1]/div/div/div/div[2]/div['
+                                                 '1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div['
+                                                 '2]/div/div/div/div')
         tweet = (f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for "
                  f"{PROMISED_DOWN}down/{PROMISED_UP}up?")
         tweet_compose.send_keys(tweet)
         time.sleep(3)
-        tweet_button = self.driver.find_elements(By.XPATH,
-                                                 '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div['
-                                                 '2]/div/div[2]/div[1]/div/div/div/div['
-                                                 '2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button = self.driver.find_element(By.XPATH,
+                                                '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div['
+                                                '2]/div/div[2]/div[1]/div/div/div/div['
+                                                '2]/div[4]/div/div/div[2]/div[3]')
         tweet_button.click()
         time.sleep(2)
         self.driver.quit()
